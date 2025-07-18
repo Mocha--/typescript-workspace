d5d6b25ac33bc7371197779d89ab6ac6519e92fc
Xibo Wang <xibo.wang@vudoo.com>
2025-07-18 12:09:45 +1000

    add initial AI commit message generation

---
+++ packages/ai-commit-message/src/generate-ai-message.ts
@@ -0,0 +1,50 @@
+import { GoogleGenAI } from '@google/genai';
+
+export interface GenerateAIMessageParams {
+  pattern: string | null;
+  branchName: string | null;
+  diff: string | null;
+  geminiApiKey: string;
+  maxTokens: number;
+}
+
+export async function generateAIMessage(params: GenerateAIMessageParams) {
+  const {
+    pattern,
+    branchName,
+    diff,
+    geminiApiKey,
+    maxTokens,
+  } = params;
+
+  const model = 'gemini-2.5-flash';
+  const prompt = `git branch name is: ${branchName} and changes are: ${diff}`;
+  const googleAI = new GoogleGenAI({
+    apiKey: geminiApiKey,
+  });
+
+  /**
+   * check if the prompt is too long
+   */
+  const { totalTokens } = await googleAI.models.countTokens({
+    model,
+    contents: prompt,
+  });
+
+  if ((totalTokens ?? Infinity) > maxTokens) {
+    throw new Error('Total tokens exceed the limit')
+  }
+
+  /**
+   * send the prompt to the AI model and get the response
+   */
+  const response = await googleAI.models.generateContent({
+    model,
+    contents: prompt,
+    config: {
+      systemInstruction: `write a git commit message less than 72 characters. If the pattern ${pattern} can be found in the branch name, then prefix the message in the format of <pattern>:. Do not capitalize the first letter of the message.`,
+    },
+  });
+
+  return response.text;
+}
