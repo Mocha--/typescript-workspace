8936fe67b0f8a3ef2c16fa5531d5a361097c3a22
Xibo Wang <xibo.wang@vudoo.com>
2025-07-18 11:42:14 +1000

    add --instruction cli option

--- packages/ai-commit-message/src/cli.ts
+++ packages/ai-commit-message/src/cli.ts
@@ -4,14 +4,16 @@ import packageJson from '../package.json' with { type: 'json' };
 import { createCommand } from 'commander';
 
 const patternOptionKey = 'pattern';
+const instructionOptionKey = 'instruction';
 
-type CLIOptions = Record<typeof patternOptionKey, string>;
+type CLIOptions = Record<typeof patternOptionKey | typeof instructionOptionKey, string>;
 
 const cliOptions: CLIOptions = createCommand()
   .name('ai-commit-message')
   .description('Generate commit messages with AI from the current git diff and git branch name')
   .version(packageJson.version)
-  .option(`--${patternOptionKey} <${patternOptionKey}>`, 'Pattern to use for the commit message')
+  .option(`--${patternOptionKey} <${patternOptionKey}>`, 'if found in the branch name, then generate the message in the format of <pattern>: <commit message>')
+  .option(`--${instructionOptionKey} <${instructionOptionKey}>`, 'instruction to use for the commit message')
   .parse(process.argv)
   .opts();
 
