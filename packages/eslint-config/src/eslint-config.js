ec4c9d3d73e3c368208c838976541e43943bf5a5
Xibo Wang <xibo.wang@vudoo.com>
2025-07-23 21:02:06 +1000

    auto-detect tsconfig root dir for typed linting

--- packages/eslint-config/src/eslint-config.js
+++ packages/eslint-config/src/eslint-config.js
@@ -1,4 +1,6 @@
+import { dirname } from "node:path";
 import { globalIgnores } from "eslint/config";
+import { loadESLint } from "eslint";
 import ts from "typescript-eslint";
 import storybook from "eslint-plugin-storybook";
 import { baseConfig } from "./configs/base-config.js";
@@ -15,16 +17,18 @@ import { defaultIgnores, srcTsFiles } from "./consts.js";
  * @param {Object} params Configuration object
  * @param {string[]} [params.ignores] Array of file patterns to ignore, defaults to `[]`
  * @param {string[]} [params.sourceCodeFiles] Array of source code file patterns to lint, defaults to `srcTsFiles`
- * @param {string} [params.sourceCodeTsConfigRootDir] Root directory containing tsconfig.json for source code, need to be configured to enable typed linting; defaults to `null`
+ * @param {string} [params.sourceCodeTsConfigRootDir] Root directory containing tsconfig.json for source code, need to be configured to enable typed linting; defaults to the directory containing the ESLint config file
  * @param {string[]} [params.playwrightFiles] Array of Playwright test file patterns to lint, defaults to `[]`
  * @param {string[]} [params.nextjsFiles] Array of Next.js file patterns to lint, defaults to `[]`
  * @returns {import('typescript-eslint').FlatConfig.Config[]} Array of ESLint flat configs
  */
 export function eslintConfig(params) {
+  const eslintConfigDir = getESLintConfigDir();
+
   const {
     ignores = [],
     sourceCodeFiles = srcTsFiles,
-    sourceCodeTsConfigRootDir = null,
+    sourceCodeTsConfigRootDir = eslintConfigDir,
     nextjsFiles = [],
     playwrightFiles = [],
   } = params ?? {};
@@ -61,4 +65,17 @@ export function eslintConfig(params) {
     ...storybook.configs["flat/recommended"],
     globalIgnores([...defaultIgnores, ...ignores]),
   );
+}
+
+/**
+ * Get the directory containing the ESLint configuration file.
+ * @returns {Promise<string>} The absolute path to the directory containing the ESLint config file
+ */
+async function getESLintConfigDir() {
+  const ESLint = await loadESLint({
+    useFlatConfig: true
+  });
+  const eslint = new ESLint();
+  const configFilePath = await eslint.findConfigFile();
+  return dirname(configFilePath);
 }
