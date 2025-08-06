318a4fa29cc60d3e60e9c42002728a51fe079dc4
Xibo Wang <xibo.wang@vudoo.com>
2025-08-06 13:03:34 +1000

    disable n/no-missing-import in node eslint config

--- packages/configs/eslint-config/src/configs/node-config.js
+++ packages/configs/eslint-config/src/configs/node-config.js
@@ -4,4 +4,13 @@ import nodePlugin from "eslint-plugin-n";
 export const nodeConfig = ts.config({
   name: "mooocha-eslint-node-config",
   extends: [nodePlugin.configs['flat/recommended']],
+  rules: {
+    /**
+     * Reason to turn off:
+     * Enabled by `eslint-plugin-n/recommended`, but are better handled by TypeScript
+     *
+     * https://github.com/eslint-community/eslint-plugin-n/blob/HEAD/docs/rules/no-missing-import.md
+     */
+    'n/no-missing-import': 'off',
+  },
 });
