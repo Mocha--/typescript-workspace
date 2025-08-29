e56b5ef30cd68568a9c298b9eabb2d8f8eb4c5d4
Xibo Wang <xibo.wang@vudoo.com>
2025-08-29 14:08:23 +1000

    init resume-engine

---
+++ packages/apps/resume-engine/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  interface Person {
+    name: string;
+    age: number;
+  }
+
+  const aaa = $state<Person>({name: 'John', age: 20});
+  const isOver5 = $derived(aaa.age > 5);
+  const title = $derived(`My title is ${aaa.name}`);
+
+  $effect(() => {
+    console.info('aaa', aaa);
+  });
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button onclick={() => aaa.age += 1}>click me</button>
+<div>aaa is {aaa}</div>
+<div>isOver5 is {isOver5}</div>
