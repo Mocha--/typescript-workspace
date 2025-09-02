faba771c71335c326477bf2e122f2636df607c5b
Xibo Wang <xibo.wang@vudoo.com>
2025-09-02 16:02:02 +1000

    add vanilla-extract for styling

--- packages/apps/resume-engine/vite.config.ts
+++ packages/apps/resume-engine/vite.config.ts
@@ -1,10 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 export default defineConfig({
   plugins: [
     sveltekit(),
     tsconfigPaths(),
+    vanillaExtractPlugin(),
   ],
 });
