01775fb0ea88908ab5ec5aa668d0f02522f5e64d
Xibo Wang <xibo.wang@vudoo.com>
2025-09-03 15:59:18 +1000

    enable dynamic vanilla extract styling

--- packages/apps/resume-engine/src/routes/+page.svelte
+++ packages/apps/resume-engine/src/routes/+page.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
+  import { assignInlineVars } from '@vanilla-extract/dynamic';
   import { StormSpirit } from '@/components/template-storm-spirit';
+  import { rootPaddingVar } from '@/components/template-storm-spirit/vars.css';
   import { styles } from './page.css';
+
+  let paddingRem = $state<number>(1);
+
+  const stormSpiritStyle = $derived(`${assignInlineVars({
+    [rootPaddingVar]: `${paddingRem}rem`
+  })}`);
 </script>
 
 <div class={styles.root}>
+  <button onclick={() => paddingRem += 1}>
+    Increase padding
+  </button>
+
   <StormSpirit
+    style={stormSpiritStyle}
     title="I am Storm Spirit"
   />
 </div>
